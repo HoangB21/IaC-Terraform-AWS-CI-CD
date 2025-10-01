a671583c8b118bcb70c384f2d34e94fcbffc492d
HoangB21 <tonghoang213@gmail.com>
2025-10-01 11:58:21 +0700

    change region and use_lockfile for vpc backend

--- environments/stg/network/vpc/backend.tf
+++ environments/stg/network/vpc/backend.tf
@@ -1,9 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "hoangtong-tf-state"
-    key            = "stg/network/vpc/terraform.tfstate"
-    region         = "ap-southeast-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
+    bucket       = "hoangtong-tf-state"
+    key          = "stg/network/vpc/terraform.tfstate"
+    region       = "ap-southeast-2"
+    use_lockfile = true
   }
 }
