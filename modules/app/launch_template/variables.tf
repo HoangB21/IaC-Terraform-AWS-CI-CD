b635af004e6098bf0b5037ef33239e448b2d70ca
HoangB21 <tonghoang213@gmail.com>
2025-10-03 01:11:58 +0700

    add ami and launch_template

---
+++ modules/app/launch_template/variables.tf
@@ -0,0 +1,39 @@
+variable "name" {
+  description = "Prefix for the Launch Template name"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "AMI ID to use for the instances"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "security_group_ids" {
+  description = "List of security group IDs"
+  type        = list(string)
+  default     = []
+}
+
+variable "key_name" {
+  description = "Key pair name for SSH access"
+  type        = string
+  default     = null
+}
+
+variable "user_data" {
+  description = "User data script to configure the instance"
+  type        = string
+  default     = null
+}
+
+variable "tags" {
+  description = "Additional tags to apply to resources"
+  type        = map(string)
+  default     = {}
+}
