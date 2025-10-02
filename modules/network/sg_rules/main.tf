bc6624b6989bd853c48f7332d6b00f4b3cf73818
HoangB21 <tonghoang213@gmail.com>
2025-10-02 12:27:23 +0700

    resolve conflict (cidr and source_sg_id) in sg_rules module

--- modules/network/sg_rules/main.tf
+++ modules/network/sg_rules/main.tf
@@ -1,9 +1,20 @@
-resource "aws_security_group_rule" "this" {
+resource "aws_security_group_rule" "this_cidr" {
+  count             = var.source_security_group_id == null ? 1 : 0
+  type              = var.type
+  from_port         = var.from_port
+  to_port           = var.to_port
+  protocol          = var.protocol
+  cidr_blocks       = var.cidr_blocks
+  security_group_id = var.security_group_id
+  description       = var.description
+}
+
+resource "aws_security_group_rule" "this_sg" {
+  count                    = var.source_security_group_id != null ? 1 : 0
   type                     = var.type
   from_port                = var.from_port
   to_port                  = var.to_port
   protocol                 = var.protocol
-  cidr_blocks              = var.cidr_blocks
   security_group_id        = var.security_group_id
   source_security_group_id = var.source_security_group_id
   description              = var.description
