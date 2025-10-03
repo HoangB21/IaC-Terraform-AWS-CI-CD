c0fd982576e0ed978683e302cc0f34cdf737a833
HoangB21 <tonghoang213@gmail.com>
2025-10-03 10:28:48 +0700

    add code for asg module, add asg in stg env

---
+++ environments/stg/app/asg/main.tf
@@ -0,0 +1,41 @@
+# Data
+data "terraform_remote_state" "subnets" {
+  backend = "s3"
+  config = {
+    bucket = "hoangtong-tf-state"
+    key    = "stg/network/subnet/terraform.tfstate"
+    region = "ap-southeast-2"
+  }
+}
+
+data "terraform_remote_state" "ec2" {
+  backend = "s3"
+  config = {
+    bucket = "hoangtong-tf-state"
+    key    = "stg/app/ec2/terraform.tfstate"
+    region = "ap-southeast-2"
+  }
+}
+
+data "terraform_remote_state" "alb" {
+  backend = "s3"
+  config = {
+    bucket = "hoangtong-tf-state"
+    key    = "stg/app/alb/terraform.tfstate"
+    region = "ap-southeast-2"
+  }
+}
+
+module "asg" {
+  source = "../../../../modules/app/asg"
+
+  name                      = "stg-asg"
+  launch_template_id        = data.terraform_remote_state.ec2.outputs.launch_template_id
+  subnet_ids                = data.terraform_remote_state.subnets.outputs.public_subnet_ids
+  target_group_arns         = [data.terraform_remote_state.alb.outputs.target_group_arn]
+  desired_capacity          = 2
+  min_size                  = 1
+  max_size                  = 3
+  target_cpu_utilization    = 50
+  health_check_grace_period = 300
+}
