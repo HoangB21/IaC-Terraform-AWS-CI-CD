453b2c46ea6abea1f397244fcf14fa288375172a
HoangB21 <tonghoang213@gmail.com>
2025-10-14 21:07:43 +0700

    fix cloudfront

--- environments/stg/network/cloudfront/outputs.tf
+++ environments/stg/network/cloudfront/outputs.tf
@@ -1,8 +1,3 @@
-output "fe_storage_bucket_name" {
-  description = "The name of the S3 bucket used for storing FE build files."
-  value       = aws_s3_bucket.fe_storage.bucket
-}
-
 output "cloudfront_distribution_id" {
   description = "The ID of the created CloudFront distribution."
   value       = module.cloudfront.cloudfront_distribution_id
