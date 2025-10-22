42f759ae1eade7bb247ce0fcb32e3f4ba3961e5f
HoangB21 <tonghoang213@gmail.com>
2025-10-23 00:04:07 +0700

    fix launch template

--- environments/stg/app/asg/main.tf
+++ environments/stg/app/asg/main.tf
@@ -34,7 +34,7 @@ module "asg" {
   launch_template_version = data.terraform_remote_state.launch_template.outputs.launch_template_latest_version
   subnet_ids              = data.terraform_remote_state.subnets.outputs.private_subnet_ids
   target_group_arns       = [data.terraform_remote_state.alb.outputs.target_group_arn]
-  desired_capacity        = 3
+  desired_capacity        = 4
   min_size                = 3
   max_size                = 5
 
