275523d5c29672ed6ee343857a0823327ac09158
HoangB21 <tonghoang213@gmail.com>
2025-10-23 01:00:11 +0700

    undo

--- environments/stg/app/launch_template/main.tf
+++ environments/stg/app/launch_template/main.tf
@@ -38,7 +38,7 @@ resource "aws_ami_from_instance" "this" {
 module "launch_template" {
   source               = "../../../../modules/app/launch_template"
   ami_id               = aws_ami_from_instance.this.id
-  instance_type        = "t3.small"
+  instance_type        = "t3.micro"
   name                 = "backend-template"
   key_name             = "hoang-key-pair"
   user_data            = <<-EOT
